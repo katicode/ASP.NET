d20aca4c31001f75f4df2548d460b8c36108b836
katicode <katimattila@outlook.com>
2019-09-05 10:51:07 +0300

    Section 2 exercise done

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models; // jotta Customers luokka löydetään
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            // luodaan objekti customers ja asetetaan sen sisällöksi GetCustomers
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details
+        public ActionResult Details(int id)
+        {
+            // c = Customer instanssi (voisi käyttää myös esim. foreach)
+            // SingleOrDefault palauttaa nollan jos asiakkaita ei löydy
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            // jos id:tä ei ole olemassa eli mennään osoitteeseen details/100 niin palautetaan HttpNotFound
+            if (customer == null)
+                return HttpNotFound();
+
+            //palautetaan Customer
+            return View(customer);
+        }
+
+        // luodaan lista, joka viedään Index-metodille ja palautetaan siellä View:lle
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,24 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // tehdään lista elokuvista, joka yllä annetaan View:lle
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -41,6 +59,8 @@ namespace Vidly.Controllers
         // Tätä actionia kutsutaan kun navigoidaan /movies -osoitteeseen
         // int? -> tekee kyseisestä parametrista valinnasen (optional, tehdään siitä nullable)
         // string sortBy = on C# reference type = nullable jo valmiiksi
+        
+/* Tämä index kommentteihin -> teen uuden
         public ActionResult Index(int? pageIndex, string sortBy)
         {
             // Jos pageIndex ei ole määritelty niin annetaan sen arvoksi 1
@@ -55,6 +75,7 @@ namespace Vidly.Controllers
             // String (isolla) vaati lisäämään ylös using.System
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+*/
 
         // regex = regular expression
         // jos tarvitsee määritellä lisää rajauksia niin googlaa: ASP.NET MVC Attribute Route Constraints
