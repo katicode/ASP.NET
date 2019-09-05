1759281a4b4495a56f8d81f321bc0418c5a08f6d
katicode <katimattila@outlook.com>
2019-09-05 12:01:48 +0300

    code first migrations

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -9,5 +9,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        // Navigation property
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
     }
 }
