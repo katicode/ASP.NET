f8a4f7bf7ee075c09fb4a4d9c7f05daf84432649
katicode <katimattila@outlook.com>
2019-09-09 15:09:23 +0300

    action New added to Customers-controller and view new, label things

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -47,7 +47,10 @@ namespace Vidly.Controllers
             //palautetaan Customer
             return View(customer);
         }
-
+        public ActionResult New()
+        {
+            return View();
+        }
 
     }
 }
