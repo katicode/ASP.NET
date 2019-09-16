b2b7f8051c18d641405d16595a7d5051969a791a
katicode <katimattila@outlook.com>
2019-09-16 12:58:21 +0300

    basic http verbs

---
+++ AspNetCoreApi/AspNetCoreApi/Controllers/QuotesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        static List<Quote> _quotes = new List<Quote>()
+        {
+            new Quote(){Id = 0, Author = "Author1", Description="Kuvaus1", Title="Kirja1"},
+            new Quote(){Id = 1, Author = "Author2", Description="Kuvaus2", Title="Kirja2"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+
+        [HttpPost]
+        public void Post(Quote quote)
+        {
+            _quotes.Add(quote);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, Quote quote)
+        {
+            _quotes[id] = quote;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _quotes.RemoveAt(id);
+        }
+    }
+
+
+}
---
+++ AspNetCoreApi/AspNetCoreApi/Models/Quote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreApi.Models
+{
+    public class Quote
+
+        // Models -luomisen jälkeen kannattaa aina "buildata" sovellus ennen kuin jatkaa. Että kaikki meni ok.
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
